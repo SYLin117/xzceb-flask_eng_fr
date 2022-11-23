d89824df786b0d1e25f870ccd213c1240611cb99
Ian Lin <newianlin@gmail.com>
2022-11-23 09:09:18 -0800

    final changes

--- machinetranslation/tests/tests.py
+++ machinetranslation/tests/tests.py
@@ -8,12 +8,14 @@ class Testing(unittest.TestCase):
         result = translator.english_to_french(a)
         b = 'Bonjour'
         self.assertEqual(result, b)
+        self.assertNotEqual(a, b, "message not equal")
 
     def test_frenchToEnglish(self):
         a = 'Bonjour'
         result = translator.french_to_english(a)
         b = 'Hello'
         self.assertEqual(result, b)
+        self.assertNotEqual(a, b, "message not equal")
 
 
 if __name__ == '__main__':
